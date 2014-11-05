7565826565e09d82c8b7c6d478f27f795ad436ab
Samuel Zapolsky <samzapo@gmail.com>
2014-11-05 11:45:31 -0500

    fixed generic file paths for tests

--- Experiments/Biped/3/RUN.sh
+++ Experiments/Biped/3/RUN.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-~/Projects/Pacer/misc/SCRIPTS/clean_data.sh .
+~/Projects/Pacer/SCRIPTS/clean_data.sh .
 
 moby-driver -y=osg -v=10 -mt=10 -s=0.001 -p=libLinksPlugin.so ./MODELS/atlas.xml > out.log 2> err.log
-~/Projects/Pacer/misc/SCRIPTS/parse_data.sh
+~/Projects/Pacer/SCRIPTS/parse_data.sh
